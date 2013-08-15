3575adee53ef524a89333f44444bb697f98e656f
palmd <palmd@users.noreply.github.com>
2013-08-15 01:46:50 -0400

    Update alert.cpp

--- src/alert.cpp
+++ src/alert.cpp
@@ -16,7 +16,7 @@ using namespace std;
 map<uint256, CAlert> mapAlerts;
 CCriticalSection cs_mapAlerts;
 
-static const char* pszMainKey = "043fa441fd4203d03f5df2b75ea14e36f20d39f43e7a61aa7552ab9bcd7ecb0e77a3be4585b13fcdaa22ef6e51f1ff6f2929bec2494385b086fb86610e33193195";
+static const char* pszMainKey = "044b87930b9bc8f049b044f6d6298f42130b3f16338f33e9855401e8b129366e2c432b5f54ac0821a0b563e5abe6e3ba29503a80a008b07a93288e0246ab4f8bda";
 
 // TestNet alerts pubKey
 static const char* pszTestKey = "0471dc165db490094d35cde15b1f5d755fa6ad6f2b5ed0f340e3f17f57389c3c2af113a8cbcc885bde73305a553b5640c83021128008ddf882e856336269080496";
