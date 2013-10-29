5f2a5cf6c72f57be3658a352016005f76bed9624
Gavin Andresen <gavinandresen@gmail.com>
2013-10-29 11:16:27 +1000

    Bug fix: CDataStream::GetAndClear() when nReadPos > 0
    Changed CDataStream::GetAndClear() to use the most obvious
    get get and clear instead of a tricky swap().
    
    Added a unit test for CDataStream insert/erase/GetAndClear.
    
    Note: GetAndClear() is not performance critical, it is used only
    by the send-a-message-to-the-network code. Bug was not noticed
    before now because the send-a-message code never erased from the
    stream.
    
    Conflicts:
    	src/test/serialize_tests.cpp

--- src/serialize.h
+++ src/serialize.h
@@ -999,8 +999,8 @@ public:
     }
 
     void GetAndClear(CSerializeData &data) {
-        vch.swap(data);
-        CSerializeData().swap(vch);
+        data.insert(data.end(), begin(), end());
+        clear();
     }
 };
 
