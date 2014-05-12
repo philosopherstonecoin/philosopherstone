3f4d144ad4f25cb570bf055cdda7bf7cca6308e4
Palm Detroit <palmdetroit@gmail.com>
2014-05-12 03:41:44 -0400

    vamp peer fix

--- src/version.h
+++ src/version.h
@@ -31,6 +31,9 @@ static const int PROTOCOL_VERSION = 60009;
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
 
+// disconnect from peers older than this proto version
+static const int MIN_PEER_PROTO_VERSION = 60008;
+
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
@@ -48,6 +51,6 @@ static const int MEMPOOL_GD_VERSION = 60002;
 #define DISPLAY_VERSION_MAJOR       1
 #define DISPLAY_VERSION_MINOR       6
 #define DISPLAY_VERSION_REVISION    0
-#define DISPLAY_VERSION_BUILD       0
+#define DISPLAY_VERSION_BUILD       1
 
 #endif
