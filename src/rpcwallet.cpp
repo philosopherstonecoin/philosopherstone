d4529f81366034bedadca05fd1ab84958ed939e5
Palm Detroit <palmdetroit@gmail.com>
2014-07-16 02:57:43 -0400

    RPC: gettransaction output format fixes

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1292,7 +1292,6 @@ Value gettransaction(const Array& params, bool fHelp)
         uint256 hashBlock = 0;
         if (GetTransaction(hash, tx, hashBlock))
         {
-            entry.push_back(Pair("txid", hash.GetHex()));
             TxToJSON(tx, 0, entry);
             if (hashBlock == 0)
                 entry.push_back(Pair("confirmations", 0));
@@ -1306,8 +1305,6 @@ Value gettransaction(const Array& params, bool fHelp)
                     if (pindex->IsInMainChain())
                     {
                         entry.push_back(Pair("confirmations", 1 + nBestHeight - pindex->nHeight));
-                        entry.push_back(Pair("txntime", (boost::int64_t)tx.nTime));
-                        entry.push_back(Pair("time", (boost::int64_t)pindex->nTime));
                     }
                     else
                         entry.push_back(Pair("confirmations", 0));
