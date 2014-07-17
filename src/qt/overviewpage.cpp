973d7d686e33bc1f6660a795ae1f756f269edd29
Palm Detroit <palmdetroit@gmail.com>
2014-07-17 06:28:06 -0400

    QT: sort transactions by date;

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -182,7 +182,7 @@ void OverviewPage::setWalletModel(WalletModel *model)
         filter->setSourceModel(model->getTransactionTableModel());
         filter->setLimit(NUM_ITEMS);
         filter->setDynamicSortFilter(true);
-        filter->setSortRole(Qt::EditRole);
+        filter->setSortRole(TransactionTableModel::DateRole);
         filter->sort(TransactionTableModel::Status, Qt::DescendingOrder);
 
         ui->listTransactions->setModel(filter);
