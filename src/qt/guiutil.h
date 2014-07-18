46c8a614d41c593b7a845cd478116132710c8310
Palm Detroit <palmdetroit@gmail.com>
2014-07-17 20:30:26 -0400

    ClickableLabel class

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -4,6 +4,8 @@
 #include <QString>
 #include <QObject>
 #include <QMessageBox>
+#include <QLabel>
+
 
 QT_BEGIN_NAMESPACE
 class QFont;
@@ -12,6 +14,7 @@ class QWidget;
 class QDateTime;
 class QUrl;
 class QAbstractItemView;
+class QLabel;
 QT_END_NAMESPACE
 class SendCoinsRecipient;
 
@@ -115,6 +118,22 @@ namespace GUIUtil
         QString header;
         QString coreOptions;
         QString uiOptions;
+    };
+
+    class ClickableLabel : public QLabel
+    {
+
+    Q_OBJECT
+
+    public:
+        explicit ClickableLabel( const QString& text ="", QWidget * parent = 0 );
+        ~ClickableLabel();
+
+    signals:
+        void clicked();
+
+    protected:
+        void mouseReleaseEvent ( QMouseEvent * event );
     };
 
 } // namespace GUIUtil
