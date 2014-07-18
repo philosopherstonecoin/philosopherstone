83cc5119f3a3a43cf1094d9fd2d1e4359a69e76a
Palm Detroit <palmdetroit@gmail.com>
2014-07-17 21:04:16 -0400

    Fix resendtx

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1775,7 +1775,7 @@ Value resendtx(const Array& params, bool fHelp)
             "Re-send unconfirmed transactions.\n"
         );
 
-    ResendWalletTransactions();
+    ResendWalletTransactions(true);
 
     return Value::null;
 }
